068594b414a5a80f8a3bc83239e40180d51cd875
kate <01redcat@mail.ru>
2023-06-12 21:53:14 +0300

    task2 16 numbers done

---
+++ Seminar2/Task2.py
@@ -0,0 +1,23 @@
+#Напишите программу, которая получает целое число и возвращает его 
+#шестнадцатеричное строковое представление. Функцию hex используйте для проверки своего результата.
+import re
+num = 189
+def func_hex(num):
+    result = []
+    letters = ['a', 'b', 'c', 'd', 'i', 'f']
+    
+    while num > 0:
+        x = num % 16
+        
+        if x >= 10:
+            x = letters[int(x-10)] 
+            result.append(str(x))
+        else:
+            result.append(str(x))
+        num = num // 16
+    result.reverse()
+    return 'ответ = %s' % ''.join(result)
+  
+
+print(func_hex(num))
+    
