85d9a8137de2a321c239a36551972583e084fdaa
EkaterinaKrasnozhen <108542392+EkaterinaKrasnozhen@users.noreply.github.com>
2023-06-12 11:56:27 +0300

    Apply suggestions from code review
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar1/Task2.py
+++ Seminar1/Task2.py
@@ -13,9 +13,9 @@ if a + b < c or a + c < b or b + c < a:
     print("треугольника с такими сторонами не существует")
 elif a == b == c:
     print("равносторонний")
-elif a == b or b == c or a == c:
-    print("равнобедренный")
-else:
+elif a != b != c:
     print("разносторонний")
+else:
+    print("равнобедренный")
 
     
