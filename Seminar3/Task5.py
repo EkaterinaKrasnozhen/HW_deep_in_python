6b445bf9b54edef59132bec1793251946977075c
EkaterinaKrasnozhen <108542392+EkaterinaKrasnozhen@users.noreply.github.com>
2023-06-23 10:33:10 +0300

    Apply suggestions from code review
    %2
    
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar3/Task5.py
+++ Seminar3/Task5.py
@@ -7,7 +7,7 @@
 my_list = [1, 1, 4, 4, 3, 3, 7, 8, 9]
 new_list = []
 for i, item in enumerate(my_list, start=1):
-    if not item % 2 == 0:    
+    if item % 2 != 0:
         new_list.append(i)
 
 print(new_list) #[1, 2, 5, 6, 7, 9]
