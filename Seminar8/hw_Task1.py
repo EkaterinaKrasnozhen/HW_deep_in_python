b904fbe219efd9bcc01e37fde93e289961130d3a
kate <01redcat@mail.ru>
2023-07-03 22:57:52 +0300

    работа с pickle, csv. json

--- Seminar8/hw_Task1.py
+++ Seminar8/hw_Task1.py
@@ -11,11 +11,7 @@ def from_pickle_to_csv(pickle_file, new_csv_file):
             open(new_csv_file, 'a') as f_write
         ):
             data_pickle = pickle.load(f_read)
-            #print(data_pickle)
-            # data = []
-            # for item in data_pickle:
-            #     for gread, id_, name, hash_ in item.items():
-            #         data.append({'gread': gread, 'id':id_, 'name': name,})
+
             cvs_write = csv.DictWriter(f_write, fieldnames=['gread', 'id_', 'name', 'hash'])
             cvs_write.writeheader()
             cvs_write.writerows(data_pickle)
